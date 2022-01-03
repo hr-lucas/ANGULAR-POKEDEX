4f67452c66b01e0c5060558a320089a6391e67ba
Lucas Henrique <lucashenriqueribeiro2009@gmail.com>
2022-01-03 01:20:43 -0300

    FirtsAdd

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+
+  constructor(
+    private pokeApiService:PokeApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe(
+      res => console.log(res)
+    );
+  }
+
+}
