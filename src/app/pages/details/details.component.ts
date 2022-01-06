027bd96f9a9dc6f09cca1fc30abe3355df27211e
lucashenriqueribeiro2009@gmail.com <lucashenriqueribeiro@MacBook-Air-de-Lucas.local>
2022-01-06 00:42:39 -0300

    Add3Commit

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+// Serviçes
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlname: string = 'https://pokeapi.co/api/v2/pokemon-species'
 
-  constructor() { }
+  public pokemon: any;
+  public isLoading: boolean = false;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.getpokemon;
+  }
+
+  get getpokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemos(`${this.urlPokemon}/${id}`)
+    const name =  this.pokeApiService.apiGetPokemos(`${this.urlname}/${id}`)
+
+    return forkJoin([pokemon, name]).subscribe(
+      res =>{
+        this.pokemon = res;
+        this.isLoading = true;
+      }
+    )
+
   }
 
 }
